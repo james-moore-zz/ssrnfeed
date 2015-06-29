674e802c3ab326ec84f57309c33a6474150301b1
James Moore <james.moore@law.duke.edu>
2015-06-29 17:07:55 -0400

    SSRN feed printer

--- ssrnfeed.py
+++ ssrnfeed.py
@@ -5,3 +5,24 @@ Open each SSRN feed
 Read each SSRN feed
 Return a time-sorted single feed
 """
+
+import csv
+import requests
+import io
+import feedparser
+import time
+import pprint
+
+x = requests.get('https://docs.google.com/spreadsheets/d/1XF9S33GnFBqh2B4lABzYkBxNAtXmia-qAiGtCxX1a8s/export?gid=0&format=csv')
+f = io.StringIO(x.text)
+reader = csv.DictReader(f)
+print(reader.fieldnames)
+
+for row in reader:
+    # print(row['rss'])
+    d = feedparser.parse(row['rss'])
+    pprint.pprint(d['feed'])
+    pprint.pprint(d['feed']['author'])
+    #for entry in d.entries:
+    #    print(entry.title)
+    time.sleep(2.0)
