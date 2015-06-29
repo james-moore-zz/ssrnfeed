785ba27369092ea0f25cbab749e11ef37268f0c6
Sean Chen <schen@law.duke.edu>
2015-06-29 17:24:36 -0400

    Encapsulated CSV function

--- ssrnfeed.py
+++ ssrnfeed.py
@@ -13,16 +13,26 @@ import feedparser
 import time
 import pprint
 
-x = requests.get('https://docs.google.com/spreadsheets/d/1XF9S33GnFBqh2B4lABzYkBxNAtXmia-qAiGtCxX1a8s/export?gid=0&format=csv')
-f = io.StringIO(x.text)
-reader = csv.DictReader(f)
-print(reader.fieldnames)
+def get_ssrn_csv():
+    """Fetches CSV file from Google; returns a CSV reader"""
+    x = requests.get('https://docs.google.com/spreadsheets/d/1XF9S33GnFBqh2B4lABzYkBxNAtXmia-qAiGtCxX1a8s/export?gid=0&format=csv')
+    f = io.StringIO(x.text)
+    reader = csv.DictReader(f)
+    # print(reader.fieldnames)
+    return reader
 
-for row in reader:
-    # print(row['rss'])
-    d = feedparser.parse(row['rss'])
-    pprint.pprint(d['feed'])
-    pprint.pprint(d['feed']['author'])
-    #for entry in d.entries:
-    #    print(entry.title)
-    time.sleep(2.0)
+def main():
+    """For each CSV row: do something"""
+    reader = get_ssrn_csv()
+    for row in reader:
+        # print(row['rss'])
+        d = feedparser.parse(row['rss'])
+        pprint.pprint(d['feed'])
+        pprint.pprint(d['feed']['author'])
+        #for entry in d.entries:
+        #    print(entry.title)
+        time.sleep(2.0)
+    return None
+
+if __name__ == '__main__':
+    main()
